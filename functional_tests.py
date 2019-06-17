98e61d27e6db6188b7b098865be1d395cc30f57a
RedPenguin101 <james.masterson101@gmail.com>
2019-06-16 19:07:17 -0500

    5.3 refactor test check row to helper method

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # edith has learned about your new todo app. she goes to the page
         self.browser.get('http://localhost:8000')
@@ -36,10 +41,7 @@ class NewVisitorTest(unittest.TestCase):
         # the item in the list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # there is still a text box, and she enters another item, enters
         # "use peacock feathers to make a fly"
@@ -56,12 +58,10 @@ class NewVisitorTest(unittest.TestCase):
         # both items one after the other
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',
-                      [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+                '2: Use peacock feathers to make a fly'
+                )
 
         # edith wonders whether the site will remember her list she sees the
         # site has generated a unique URL for her - there is explanatory
