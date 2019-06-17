1799fff89759ae4cf4260ffe975c33a3d53e2fef
Joe <james.masterson101@gmail.com>
2019-06-17 18:23:09 -0500

    replace voodoo timesleeps with wait for function

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,8 +3,12 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
 import time
 import unittest
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -14,10 +18,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # edith has learned about your new todo app. she goes to the page
@@ -41,8 +53,7 @@ class NewVisitorTest(LiveServerTestCase):
         # when she hits enter the page updates and now the page lists
         # the item in the list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # there is still a text box, and she enters another item, enters
         # "use peacock feathers to make a fly"
@@ -58,9 +69,8 @@ class NewVisitorTest(LiveServerTestCase):
         # when she hits enter the page updates and now the page lists
         # both items one after the other
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table(
                 '2: Use peacock feathers to make a fly'
                 )
 
